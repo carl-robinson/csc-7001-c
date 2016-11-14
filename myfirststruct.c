c2839e07077e7020878aa6209c1d046b6a56ab80
Carl <carl.robinson@gmail.com>
2016-11-14 15:48:21 +0100

    Updated myfirststruct.c with exercise 3

--- myfirststruct.c
+++ myfirststruct.c
@@ -11,12 +11,22 @@
 //main function
 int main ( int argc, char * argv []) {
 
-    struct complex {
+    typedef struct person {
+        unsigned char name[80];
+        struct person * mother;
+        struct person * father;
+    } person;
+
+    typedef struct complex {
     
         double dblReal;
         double dblImaginary;
     
-    } complex1;
+    } complex;
+
+    typedef unsigned char u_char;
+    typedef int * pint;
+
     
     union {
 
@@ -24,6 +34,8 @@ int main ( int argc, char * argv []) {
         unsigned char array [8];
 
     } myunion;
+
+    typedef int (*func_ptr_array[4])(int);
 
     //Print welcome message and ask for a guess of the number
     printf("Hi, please enter a number with a decimal point, and I'll give you the 4 bytes that make it up!\n");
