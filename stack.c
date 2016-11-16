6adfb3bae71496b1e95e0e54fd8eb0c08b4e1f23
Carl <carl.robinson@gmail.com>
2016-11-16 15:44:10 +0100

    Updated stack.c

--- stack.c
+++ stack.c
@@ -14,10 +14,17 @@ struct element {
 
 typedef struct element * stack; //stack is a pointer to a struct element
 
+stack new ();
+int isempty(stack s);
+stack push(stack s, int i);
+stack pop(stack s);
+int top(stack s);
+
+
 //////////////////////////////////////////
 //new stack function
 //returns a null valued pointer to a struct element i.e. the start of a stack
-stack * new () {
+stack new () {
     return NULL;
 }
 
@@ -42,24 +49,57 @@ stack push(stack s, int i){
     e = (struct element *) malloc(sizeof(struct element));
     e->val = i;
     e->link = s;
-    s = e;
-    return s;
+    printf("Pushed %d to stack\n",i);
+    printf("Top of stack is: %d\n",i);
+    return e;
 }
 ///////////////////////////////////////
 //pop function
 stack pop(stack s){
+    if (isempty(s)){
+        printf("Cannot pop. Stack is empty.\n");
+        return NULL;
+    }
+    free(s);
+    printf("Popped top of stack\n");
+    printf("Top of stack is: %d\n",s->link->val);
     return s->link;
 }
 ///////////////////////////////////////
 //top function
 int top(stack s){
+    if (isempty(s)){
+        printf("Cannot pop. Stack is empty.\n");
+        return 0;
+    }
+    printf("Top of stack is: %d\n",s->val);
     return s->val;
 }
 ///////////////////////////////////////////////
 //main function
 int main ( int argc, char * argv []) {
 
-   return 0; 
+    stack s = new();
+    int i;
+    
+    for(i=1;i<=5;i++){
+        s = push(s,i);
+    }
+
+    s = pop(s);
+    s = pop(s);
+    s = pop(s);
+    s = push(s,6);
+    s = push(s,7);
+    s = pop(s);
+    s = push(s,8);
+    s = push(s,9);
+    s = push(s,10);
+    s = push(s,11);
+    s = push(s,12);
+    s = pop(s);
+
+    return 0; 
 
 }// end of main function
 
