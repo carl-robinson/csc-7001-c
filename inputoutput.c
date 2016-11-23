f1b4476220469fafc77a7fa08051dcc6cd3092dc
Carl <carl.robinson@gmail.com>
2016-11-23 17:18:43 +0100

    Finished basic exercise for one file

--- inputoutput.c
+++ inputoutput.c
@@ -1,8 +1,11 @@
 /* inputoutput.c */
 
 # include <stdio.h>
-# include <fcnt1.h>
-# define BUFSIZE 1024
+# include <stdlib.h>
+# include <ctype.h>
+# include <fcntl.h>
+# include <unistd.h>
+# define S_IREAD 0400 
 
 //use SYSTEMCALLS
 int main ( int argc, char * argv []) {
@@ -13,24 +16,50 @@ int main ( int argc, char * argv []) {
 int fd;
 
 //create counters
-int char_counter, newline_counter, word_counter;
+int newline_counter = 0, char_counter = 0, word_counter = 0;
 
-//create a buffer for characters
-char buffer [BUFSIZE]; 
+//create buffers for characters
+char buffer, ch; 
+int state_last = 1, state = 1;
 
 //open the file
-if(fd = open(argv[0],O_RDONLY|O_TEXT,S_IREAD < 0){
+fd = open(argv[1],O_RDONLY,S_IREAD); 
+if(fd < 0){
     perror("Unable to open data");
     exit (1);
 }
+printf("loaded file\n");
 
 //Read all charactes in file - stop when read returns 0 (EOF)
-while ( (length = read (fd, buffer, 1)) > 0 ){
+while ( (ch = read(fd, &buffer, 1)) > 0 ){
     //increment char_counter for each char read
+    if(buffer != '\n'){
+        char_counter++;
+    }
 
     //increment newline_counter for each \n read
+    if(buffer == '\n'){
+        newline_counter++;
+    }
+
     //increment word_counter if FSM flip flops 
+    //if(buffer isalpha change sign of state bit)
+    if(!isalnum(buffer)){
+        state *= -1; 
+        //printf("state changed\n",word_counter);
+    }
+    //test state bit - if different to what it was, increment counter
+    if(state != state_last){
+        //set state_last to state
+        state_last = state;
+        //increment word counter
+        word_counter++;
+    }
 }
 
-close (fd);
-exit (0); 
+printf("char_counter = %d\n",char_counter);
+printf("newline_counter = %d\n",newline_counter);
+printf("word_counter = %d\n",word_counter);
+close(fd);
+exit(0);
+}
