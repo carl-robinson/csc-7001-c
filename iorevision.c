8212cef70d6e3f454c12a4383cf7fdaa05a32e43
Carl <carl.robinson@gmail.com>
2017-01-22 18:12:13 +0100

    Updated with variable fix

--- iorevision.c
+++ iorevision.c
@@ -10,7 +10,7 @@ int main ( int argc, char * argv []) {
     //keep doing that until EOF sent with ^D
     
     //declare vars
-    char c;
+    char c,ch;
 
     //write a string to a file
     FILE * fdw;
